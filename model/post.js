d6a1e25342fa6c4fc8a0805aab1d2fe469565de4
klu0926 <lukuoyu@gmail.com>
2025-03-23 11:40:54 -0400

    create Post class

---
+++ model/post.js
@@ -0,0 +1,19 @@
+// Default image
+const defaultImage = "https://placehold.co/800x400"
+
+
+// create a new post
+class Post {
+  constructor(title = 'New Post', content, cover) {
+    this.id = this.generateId();
+    this.title = title;
+    this.content = content || ""
+    this.cover = cover || defaultImage;
+    this.created = new Date()
+  }
+  generateId() {
+    const length = 9;
+    return Math.random().toString(36).slice(2, 2 + length); // cleaner and skips split
+  }
+}
+export default Post;
