9a1de233ae799b2ee689060e35728dc079cf7db6
klu0926 <lukuoyu@gmail.com>
2025-03-23 12:16:06 -0400

    Add link to each post, and add a Create button to new-post.html

--- index.js
+++ index.js
@@ -8,26 +8,30 @@ class View {
   displayPosts(posts) {
     const postsDiv = document.querySelector('#posts')
     posts.forEach(post => {
-      const postDiv = document.createElement('div')
+      const postAnchor = document.createElement('a')
       const titleDiv = document.createElement('div')
-      const contentPre = document.createElement('pre')
+      const contentP = document.createElement('p')
       const imageDiv = document.createElement('div')
       const image = document.createElement('img')
 
       titleDiv.textContent = post.title
-      contentPre.textContent = post.content
+      contentP.textContent = post.content
       image.src = post.cover
 
-      postDiv.appendChild(imageDiv)
-      postDiv.appendChild(titleDiv)
-      postDiv.appendChild(contentPre)
+      postAnchor.appendChild(imageDiv)
+      postAnchor.appendChild(titleDiv)
+      postAnchor.appendChild(contentP)
       imageDiv.appendChild(image)
-      postsDiv.appendChild(postDiv)
+      postsDiv.appendChild(postAnchor)
+
+      // add link
+      postAnchor.href = `post.html/?postId=${post.id}`
 
       // add class
-      postDiv.classList.add('post')
+      imageDiv.classList.add('image-div')
+      titleDiv.classList.add('title')
+      postAnchor.classList.add('post')
     })
-
   }
 }
 
@@ -56,10 +60,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const controller = new Controller(model, view)
   controller.init()
 
-
-  document.querySelector('#create').addEventListener('click', () => {
-    model.createPost(new Post('New Post', 'This is a example post'))
+  // Generate a post
+  document.querySelector('#generate').addEventListener('click', () => {
+    model.createPost(new Post('New Post', 'This is a randomly generated post'))
     window.location.reload()
+  })
+
+
+  // Create a New Post
+  document.querySelector('#create').addEventListener('click', () => {
+    window.location.href = 'new-post.html'
   })
 
 })
